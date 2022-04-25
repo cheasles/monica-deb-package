d413bdf5f704a0382ec814917cdbbedddbe14594
cheasles <cheasles@github.com>
2022-04-26 11:49:55 +1200

    Squashed 'source/' content from commit a07317f3
    git-subtree-dir: source
    git-subtree-split: a07317f309f25772925436346b4ebb23b6e69b43

---
+++ app/Models/Contact/Message.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models\Contact;
+
+use App\Traits\HasUuid;
+use App\Models\Account\Account;
+use App\Models\ModelBindingHasher as Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasUuid;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['written_at'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'written_by_me' => 'boolean',
+    ];
+
+    /**
+     * Get the account record associated with the message.
+     *
+     * @return BelongsTo
+     */
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    /**
+     * Get the contact record associated with the message.
+     *
+     * @return BelongsTo
+     */
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+    /**
+     * Get the Conversation records associated with the message.
+     *
+     * @return BelongsTo
+     */
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+}
